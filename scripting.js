6ffbda832a03b3bce04879c066ab59a80df1e197
Harper Rhett <harperrhett@gmail.com>
2020-12-30 15:28:54 -0500

    Added spaces to text

--- scripting.js
+++ scripting.js
@@ -6,11 +6,11 @@ function exposeText(id) {
 function getText(id) {
 	switch(id) {
 		case "Distrakt":
-			return "Distrakt was the first project I worked on as a part of Auburn's game developement club, TigerDev."
-				+ "The idea, formulated by my buddy Holden and I, was to create a game where the player was constantly distracted."
-				+ "We thought that if we took some puzzle game mechanics, and some FPS mechanics, and slapped them together, that would do just the trick."
-				+ "We recruited two others from the club, and proceeded to start work on this 3D masterpiece."
-				+ "By the end of the semester, we were burnt out already, and the game didn't even have a first level."
+			return "Distrakt was the first project I worked on as a part of Auburn's game developement club, TigerDev. "
+				+ "The idea, formulated by my buddy Holden and I, was to create a game where the player was constantly distracted. "
+				+ "We thought that if we took some puzzle game mechanics, and some FPS mechanics, and slapped them together, that would do just the trick. "
+				+ "We recruited two others from the club, and proceeded to start work on this 3D masterpiece. "
+				+ "By the end of the semester, we were burnt out already, and the game didn't even have a first level. "
 				+ "Nevertheless, it was a cool tech demo that showed off some cool technologies such as gravity manipulation, so we dropped the project and went on our merry way.";
 			break;
 		
