98066e010c2605ab315b83e0e4563022a72efc62
Harper Rhett <harperrhett@gmail.com>
2021-01-01 21:08:23 -0500

    Added some projects

--- scripting.js
+++ scripting.js
@@ -6,7 +6,9 @@ function exposeText(id) {
 function getText(id) {
 	switch(id) {
 		case "Distrakt":
-			return "Distrakt was the first project I worked on as a part of Auburn's game developement club, TigerDev. "
+			return "TigerDev<br>"
+				+ "2020<br><br>"
+				+ "Distrakt was the first project I worked on as a part of Auburn's game developement club, TigerDev. "
 				+ "The idea, formulated by my buddy Holden and I, was to create a game where the player was constantly distracted. "
 				+ "We thought that if we took some puzzle game mechanics, and some FPS mechanics, and slapped them together, that would do just the trick. "
 				+ "We recruited two others from the club, and proceeded to start work on this 3D masterpiece. "
@@ -15,35 +17,63 @@ function getText(id) {
 			break;
 		
 		case "Factorama":
-			return "Factorama";
+			return "Voltaic Studios<br>"
+				+ "2020<br><br>"
+				+ "description";
 			break;
 		
 		case "VOLAXIA":
-			return "VOLAXIA";
+			return "Harper Rhett & Giuliano De Pian<br>"
+				+ "2020<br><br>"
+				+ "description";
 			break;
 		
 		case "Bad Ass Santa":
-			return "Bad Ass Santa";
+			return "Harper Rhett, Giuliano De Pian, and Carlita<br>"
+				+ "2019<br><br>"
+				+ "description";
 			break;
 		
 		case "Leap Theory":
-			return "Leap Theory";
+			return "Harper Rhett & Jeremy Rhett"
+				+ "2019<br><br>"
+				+ "description";
 			break;
 		
 		case "FIG":
-			return "FIG";
+			return "Harper Rhett<br>"
+				+ "2020<br><br>"
+				+ "description";
 			break;
 		
 		case "Little Nightmare":
-			return "Little Nightmare";
+			return "Harper Rhett & Stranger Who's Name I Forgot"
+				+ "2019<br><br>"
+				+ "description";
 			break;
 			
 		case "Slimer":
-			return "Slimer";
+			return "Harper Rhett<br>"
+				+ "2019<br><br>"
+				+ "description";
 			break;
 			
 		case "Finally Sprinting":
-			return "Finally Sprinting";
+			return "Harper Rhett<br>"
+				+ "2020<br><br>"
+				+ "description";
+			break;
+		
+		case "Vanilla++":
+			return "Harper Rhett<br>"
+				+ "2018<br><br>"
+				+ "description";
+			break;
+		
+		case "MCPE Ramps":
+			return "Harper Rhett<br>"
+			+ "2016<br><br>"
+			+ "description";
 			break;
 		
 		default:
