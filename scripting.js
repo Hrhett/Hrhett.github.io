9fc08c29c6147f4dc7addbbdb3e89a7bf94c01c5
Harper Rhett <harperrhett@gmail.com>
2021-01-22 17:47:55 -0600

    Updated some descriptions

--- scripting.js
+++ scripting.js
@@ -8,14 +8,19 @@ function exposeText(id) {
 function getText(id) {
 	switch(id) {
 		case "Tentatron":
-			return "Tentatron<br>"
+			return "Harper Rhett & Jeremy Rhett<br>"
 				+ "2021<br><br>"
-				+ "description";
+				+ "I was starting to feel withdraw from C# mid-way through Winter break, so I decided to participate in a game jam. "
+				+ "It was fun but I made the major mistake of winging everything, and it is by far the creation I am least proud of... "
+				+ "I should have gone in with a team and a game plan."
+				+ "Oh well.";
 
 		case "Quote Bot":
-			return "Quote Bot<br>"
+			return "Harper Rhett<br>"
 				+ "2020<br><br>"
-				+ "description";
+				+ "Over Winter break I wanted to learn some Python, and I figured a cool way to do that would be to create a Reddit bot."
+				+ "Naturally, I created a bot that finished quotes from famous pop culture."
+				+ "The bot is no longer in service.";
 
 		case "Distrakt":
 			return "TigerDev<br>"
@@ -31,7 +36,12 @@ function getText(id) {
 		case "Factorama":
 			return "Voltaic Studios<br>"
 				+ "2020<br><br>"
-				+ "description";
+				+ "Factorama was my first commercial production, and quite a fantastic one at that."
+				+ "I spent the entire Summer after my senior year of high school programming this factory management game."
+				+ "I was so dedicated, it was about as time consuming as a full time job."
+				+ "I even hired up Giuliano De Pian, someone I had worked with before, to do the music."
+				+ "Several months in, and I was able to release it to the Android Play Store."
+				+ "I made about negative eighty bucks.";
 			break;
 		
 		case "VOLAXIA":
@@ -85,7 +95,9 @@ function getText(id) {
 		case "MCPE Ramps":
 			return "Harper Rhett<br>"
 			+ "2016<br><br>"
-			+ "description";
+			+ "During my freshman year of high school, I was pretty into Minecraft, and decided I wanted to make a mod."
+			+ "I targeted Minecraft Pocket Edition because the developers were coming out with a new addon system I wanted to try out."
+			+ "All I had to do was mess with some config files, but it was my first real developer experience.";
 			break;
 		
 		default:
